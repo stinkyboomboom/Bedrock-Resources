d9212019584d47b2be7d62f43806da8234620bd3
AlexVR <josian.velez@upr.edu>
2019-10-30 15:55:32 -0500

    ender hush particles and offset

--- src/main/java/com/alexvr/bedres/blocks/EnderHush.java
+++ src/main/java/com/alexvr/bedres/blocks/EnderHush.java
@@ -4,9 +4,15 @@ import com.alexvr.bedres.registry.ModBlocks;
 import com.alexvr.bedres.utils.References;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.Random;
 
 public class EnderHush extends FlowerBlock {
 
@@ -19,5 +25,15 @@ public class EnderHush extends FlowerBlock {
     protected boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
         Block block = state.getBlock();
         return block == ModBlocks.enderianBrick || block == Blocks.GRASS_BLOCK || block == Blocks.DIRT || block == Blocks.COARSE_DIRT || block == Blocks.PODZOL || block == Blocks.FARMLAND;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        double d0 = (double)pos.getX() + 0.5D + ((double)rand.nextFloat() - 0.5D) * 0.2D+ getOffset(stateIn,worldIn,pos).x;
+        double d1 = (double)((float)pos.getY() + 0.0625F);
+        double d2 = (double)pos.getZ() + 0.5D + ((double)rand.nextFloat() - 0.5D) * 0.2D+ getOffset(stateIn,worldIn,pos).z;
+
+
+        worldIn.addParticle(ParticleTypes.PORTAL, d0, d1, d2, 0.0D, 0.0D, 0.0D);
     }
 }
