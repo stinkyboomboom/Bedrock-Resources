cfd11434f964a598c37fcda9809b04107487ac3b
AlexVR <josian.velez@upr.edu>
2019-10-31 10:53:22 -0500

    model changes

--- src/main/java/com/alexvr/bedres/blocks/Blazium.java
+++ src/main/java/com/alexvr/bedres/blocks/Blazium.java
@@ -3,10 +3,13 @@ package com.alexvr.bedres.blocks;
 import com.alexvr.bedres.utils.References;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -33,5 +36,28 @@ public class Blazium extends FlowerBlock {
         double d2 = (double)pos.getZ() + 0.5D + ((double)rand.nextFloat() - 0.5D) * 0.2D + getOffset(stateIn,worldIn,pos).z;
 
         worldIn.addParticle(ParticleTypes.FLAME, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+    }
+
+    @Override
+    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos pos, BlockPos facingPos) {
+        if (worldIn.getBlockState(pos.offset(Direction.DOWN)).getBlock() == Blocks.AIR){
+            BlockState blockstate1 = ((FireBlock)Blocks.FIRE).getStateForPlacement(worldIn, pos.offset(Direction.NORTH));
+            worldIn.setBlockState(pos.offset(Direction.NORTH), blockstate1, 11);
+            BlockState blockstate2 = ((FireBlock)Blocks.FIRE).getStateForPlacement(worldIn, pos.offset(Direction.SOUTH));
+            worldIn.setBlockState(pos.offset(Direction.SOUTH), blockstate2, 11);
+            BlockState blockstate3 = ((FireBlock)Blocks.FIRE).getStateForPlacement(worldIn, pos.offset(Direction.EAST));
+            worldIn.setBlockState(pos.offset(Direction.EAST), blockstate3, 11);
+            BlockState blockstate4 = ((FireBlock)Blocks.FIRE).getStateForPlacement(worldIn, pos.offset(Direction.WEST));
+            worldIn.setBlockState(pos.offset(Direction.WEST), blockstate4, 11);
+            return ((FireBlock)Blocks.FIRE).getStateForPlacement(worldIn, pos);
+        }
+        return super.updatePostPlacement(stateIn, facing, facingState, worldIn, pos, facingPos);
+    }
+
+    @Override
+    public void onBlockHarvested(World worldIn, BlockPos pos, BlockState state, PlayerEntity player) {
+        player.setFire(15);
+
+        super.onBlockHarvested(worldIn, pos, state, player);
     }
 }
