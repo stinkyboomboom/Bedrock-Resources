9ec2551553be1e1e98d55df583389cd3e6f804ac
Josian Velez Ramos <josian.velez@upr.edu>
2019-10-31 14:54:46 -0400

    book update

--- src/main/java/com/alexvr/bedres/gui/FluxOracleScreenGui.java
+++ src/main/java/com/alexvr/bedres/gui/FluxOracleScreenGui.java
@@ -23,6 +23,17 @@ public class FluxOracleScreenGui extends Screen {
     private boolean knife = false;
     private boolean altar = false;
     private boolean altar2 = false;
+
+    private boolean blazium = false;
+    private boolean hush = false;
+    private boolean daize = false;
+    private boolean eOre = false;
+    private boolean eIngot = false;
+    private boolean tank = false;
+    private boolean scraper = false;
+    private boolean flux = false;
+
+
     private ImageButton back;
     private ImageButton next;
 
@@ -39,82 +50,176 @@ public class FluxOracleScreenGui extends Screen {
     public boolean shouldCloseOnEsc() {
         if(BedrockResources.proxy.getMinecraft().player.getHeldItemMainhand().getItem() instanceof FluxOracle && ((FluxOracle)BedrockResources.proxy.getMinecraft().player.getHeldItemMainhand().getItem()).beingUsed) {
             ((FluxOracle)BedrockResources.proxy.getMinecraft().player.getHeldItemMainhand().getItem()).beingUsed = false;
-            main = true;
-            scrapes = false;
+            changePage("main");
         }
-
         return true;
     }
 
-
     @Override
     protected void init() {
-        back = new ImageButton(15+((Minecraft.getInstance().mainWindow.getScaledWidth()-15)/8), ((Minecraft.getInstance().mainWindow.getScaledHeight()-48)),32,32,0,0,0,new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/back.png"),32,32, (button) -> {
 
+        back = new ImageButton(15+((Minecraft.getInstance().mainWindow.getScaledWidth()-15)/8), ((Minecraft.getInstance().mainWindow.getScaledHeight()-48)),32,32,0,0,0,
+                new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/back.png"),32,32, (button) -> {
             if(altar2){
-                altar2=false;
-                altar=true;
+                changePage("altar");
             }else {
-                main = true;
-                scrapes = false;
-                knife = false;
-                altar = false;
-                altar2 = false;
+                changePage("main");
             }
-
         });
 
-        next = new ImageButton(76+((Minecraft.getInstance().mainWindow.getScaledWidth()-15)/8), ((Minecraft.getInstance().mainWindow.getScaledHeight()-48)),32,32,0,0,0,new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/next.png"),32,32, (button) -> {
-
+        next = new ImageButton(76+((Minecraft.getInstance().mainWindow.getScaledWidth()-15)/8), ((Minecraft.getInstance().mainWindow.getScaledHeight()-48)),32,32,0,0,0,
+                new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/next.png"),32,32, (button) -> {
             if (altar){
-                altar=false;
-                altar2=true;
+                changePage("altar2");
             }
-
-
         });
 
         List<AbstractButton> buttons = new ArrayList<AbstractButton>() {{
-            add(new ImageButton(15+((Minecraft.getInstance().mainWindow.getScaledWidth()-15)/8), 15+((Minecraft.getInstance().mainWindow.getScaledHeight()-15)/6),32,32,0,0,0,new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/bedrock_scrapes.png"),32,32, (button) -> {
-                if(main) {
-                    main = false;
-                    scrapes = true;
-                    knife = false;
-                    altar = false;
-                    altar2 = false;
-                }
 
+            add(new ImageButton(15+((Minecraft.getInstance().mainWindow.getScaledWidth()-15)/8), 15+((Minecraft.getInstance().mainWindow.getScaledHeight()-15)/6),32,32,0,0,0,
+                    new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/bedrock_scrapes.png"),32,32, (button) -> {
+                if(main) {
+                    changePage("scrapes");
+                }
             }));
 
-            add(new ImageButton(64+((Minecraft.getInstance().mainWindow.getScaledWidth()-64)/8), 64+((Minecraft.getInstance().mainWindow.getScaledHeight()-64)/6),32,32,0,0,0,new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/scrape_knife.png"),32,32, (button) -> {
+            add(new ImageButton(64+((Minecraft.getInstance().mainWindow.getScaledWidth()-64)/8), 64+((Minecraft.getInstance().mainWindow.getScaledHeight()-64)/6),32,32,0,0,0,
+                    new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/scrape_knife.png"),32,32, (button) -> {
                 if(main) {
-                    main = false;
-                    scrapes = false;
-                    knife = true;
-                    altar = true;
-                    altar2 = false;
+                    changePage("knife");
                 }
-
             }));
 
-            add(new ImageButton(164+((Minecraft.getInstance().mainWindow.getScaledWidth()-64)/8), 46+((Minecraft.getInstance().mainWindow.getScaledHeight()-64)/6),32,32,0,0,0,new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/altar.png"),32,32, (button) -> {
+            add(new ImageButton(164+((Minecraft.getInstance().mainWindow.getScaledWidth()-64)/8), 46+((Minecraft.getInstance().mainWindow.getScaledHeight()-64)/6),32,32,0,0,0,
+                    new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/altar.png"),32,32, (button) -> {
                 if(main) {
-                    main = false;
-                    scrapes = false;
-                    knife = false;
-                    altar = true;
-                    altar2 = false;
+                    changePage("altar");
                 }
-
             }));
+
+            add(new ImageButton(164+((Minecraft.getInstance().mainWindow.getScaledWidth()-64)/8), 46+((Minecraft.getInstance().mainWindow.getScaledHeight()-64)/6),32,32,0,0,0,
+                    new ResourceLocation(BedrockResources.MODID,"textures/blocks/blazium.png"),32,32, (button) -> {
+                if(main) {
+                    changePage("blazium");
+                }
+            }));
+
+            add(new ImageButton(164+((Minecraft.getInstance().mainWindow.getScaledWidth()-64)/8), 46+((Minecraft.getInstance().mainWindow.getScaledHeight()-64)/6),32,32,0,0,0,
+                    new ResourceLocation(BedrockResources.MODID,"textures/blocks/ender_hush_backup.png"),32,32, (button) -> {
+                if(main) {
+                    changePage("hush");
+                }
+            }));
+
+            add(new ImageButton(164+((Minecraft.getInstance().mainWindow.getScaledWidth()-64)/8), 46+((Minecraft.getInstance().mainWindow.getScaledHeight()-64)/6),32,32,0,0,0,
+                    new ResourceLocation(BedrockResources.MODID,"textures/blocks/sun_daize.png"),32,32, (button) -> {
+                if(main) {
+                    changePage("daize");
+                }
+            }));
+
+            add(new ImageButton(164+((Minecraft.getInstance().mainWindow.getScaledWidth()-64)/8), 46+((Minecraft.getInstance().mainWindow.getScaledHeight()-64)/6),32,32,0,0,0,
+                    new ResourceLocation(BedrockResources.MODID,"textures/blocks/enderian_ore.png"),32,32, (button) -> {
+                if(main) {
+                    changePage("eOre");
+                }
+            }));
+
+            add(new ImageButton(164+((Minecraft.getInstance().mainWindow.getScaledWidth()-64)/8), 46+((Minecraft.getInstance().mainWindow.getScaledHeight()-64)/6),32,32,0,0,0,
+                    new ResourceLocation(BedrockResources.MODID,"textures/items/enderian_ingot.png"),32,32, (button) -> {
+                if(main) {
+                    changePage("eIngot");
+                }
+            }));
+
+            add(new ImageButton(164+((Minecraft.getInstance().mainWindow.getScaledWidth()-64)/8), 46+((Minecraft.getInstance().mainWindow.getScaledHeight()-64)/6),32,32,0,0,0,
+                    new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/tank.png"),32,32, (button) -> {
+                if(main) {
+                    changePage("tank");
+                }
+            }));
+
+            add(new ImageButton(164+((Minecraft.getInstance().mainWindow.getScaledWidth()-64)/8), 46+((Minecraft.getInstance().mainWindow.getScaledHeight()-64)/6),32,32,0,0,0,
+                    new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/scraper.png"),32,32, (button) -> {
+                if(main) {
+                    changePage("scraper");
+                }
+            }));
+
+            add(new ImageButton(164+((Minecraft.getInstance().mainWindow.getScaledWidth()-64)/8), 46+((Minecraft.getInstance().mainWindow.getScaledHeight()-64)/6),32,32,0,0,0,
+                    new ResourceLocation(BedrockResources.MODID,"textures/gui/widget/flux.png"),32,32, (button) -> {
+                if(main) {
+                    changePage("flux");
+                }
+            }));
+
 
             add(back);
+
             add(next);
 
         }};
 
         buttons.forEach(this::addButton);
+    }
 
+    public void changePage(String name){
+
+        main = false;
+        scrapes = false;
+        knife = false;
+        altar = false;
+        altar2 = false;
+        blazium = false;
+        hush = false;
+        daize = false;
+        eOre = false;
+        eIngot = false;
+        tank = false;
+        flux = false;
+        scraper = false;
+
+        switch(name){
+            case "main":
+                main=true;
+                break;
+            case "scrapes":
+                scrapes=true;
+                break;
+            case "knife":
+                knife=true;
+                break;
+            case "altar":
+                altar=true;
+                break;
+            case "altar2":
+                altar2=true;
+                break;
+            case "blazium":
+                blazium=true;
+                break;
+            case "hush":
+                hush=true;
+                break;
+            case "daize":
+                daize=true;
+                break;
+            case "eOre":
+                eOre=true;
+                break;
+            case "eIngot":
+                eIngot=true;
+                break;
+            case "tank":
+                tank=true;
+                break;
+            case "flux":
+                flux=true;
+                break;
+            case "scraper":
+                scraper=true;
+                break;
+        }
     }
 
 
@@ -130,7 +235,7 @@ public class FluxOracleScreenGui extends Screen {
             drawModalRectWithCustomSizedTexture(15, Minecraft.getInstance().mainWindow.getScaledWidth() - 15, Minecraft.getInstance().mainWindow.getScaledHeight() - 15, 15, new ResourceLocation("bedres", "textures/gui/flux_oracle_book_info_gui.png"));
             drawModalRectWithCustomSizedTexture(5+((Minecraft.getInstance().mainWindow.getScaledWidth())/8.0f)-32, 5+ ((Minecraft.getInstance().mainWindow.getScaledWidth())/8.0), (5+((Minecraft.getInstance().mainWindow.getScaledHeight()-15)/6.0)) + 32, 5+((Minecraft.getInstance().mainWindow.getScaledHeight()-15)/6.0f), new ResourceLocation("bedres", "textures/gui/widget/bedrock_scrapes.png"));
             drawString(minecraft.fontRenderer,"Bedrock Scrapes:",12+((Minecraft.getInstance().mainWindow.getScaledWidth()-15)/8), ((Minecraft.getInstance().mainWindow.getScaledHeight()-15)/6),1111111);
-            String s = "Bedrock Scrapes are the introductory item to this mod. They are particles that were, as the name implies, scrapped from bedrock. By doing so you will inhale some bedrock particles which arent great for you until you learn to manage them. To obtain use a Scaping knife on bedrock by shift right clicking it.";
+            String s = "Bedrock Scrapes are the introductory item to this mod. They are particles that were, as the name implies, scrapped from bedrock. By doing so you will inhale some bedrock particles which arent great for you until you learn to manage them. To obtain use a Scaping knife on bedrock by shift right clicking it.Right clicking this on a block will place it down like redstone. ";
             renderString(s,12+((Minecraft.getInstance().mainWindow.getScaledWidth()-15)/8), ((Minecraft.getInstance().mainWindow.getScaledHeight()-15)/6),Minecraft.getInstance().mainWindow.getScaledWidth()-30);
         }else if (knife){
             drawModalRectWithCustomSizedTexture(15, Minecraft.getInstance().mainWindow.getScaledWidth() - 15, Minecraft.getInstance().mainWindow.getScaledHeight() - 15, 15, new ResourceLocation("bedres", "textures/gui/flux_oracle_book_info_gui.png"));
@@ -144,13 +249,15 @@ public class FluxOracleScreenGui extends Screen {
             drawString(minecraft.fontRenderer,"Altar:",12+((Minecraft.getInstance().mainWindow.getScaledWidth()-15)/8), ((Minecraft.getInstance().mainWindow.getScaledHeight()-15)/6),1111111);
             String s = "The altar is a multiblock structure composed of 4 Bedrocium Towers and 1 Bedrocium Pedestal. This structure can be used for many things, most importantly crafting and infusing items/blocks. You can right click any of the slots of the towers or the pedestal to place an item in it. Once a correct recipe is placed the output will be displayed on top of the altar. Right click the pedestal with bedrock scrapes to start crafting.";
             renderString(s,12+((Minecraft.getInstance().mainWindow.getScaledWidth()-15)/8), ((Minecraft.getInstance().mainWindow.getScaledHeight()-15)/6),Minecraft.getInstance().mainWindow.getScaledWidth()-30);
+            next.renderButton(p_render_1_, p_render_2_, p_render_3_);
+
         }else if (altar2){
             drawModalRectWithCustomSizedTexture(15, Minecraft.getInstance().mainWindow.getScaledWidth() - 15, Minecraft.getInstance().mainWindow.getScaledHeight() - 15, 15, new ResourceLocation("bedres", "textures/gui/flux_oracle_book_info_gui.png"));
             drawModalRectWithCustomSizedTexture(32, Minecraft.getInstance().mainWindow.getScaledWidth()-32, (Minecraft.getInstance().mainWindow.getScaledHeight()-32) , 32, new ResourceLocation("bedres", "textures/gui/altar.png"));
             drawString(minecraft.fontRenderer,"Altar:",12+((Minecraft.getInstance().mainWindow.getScaledWidth()-15)/8), ((Minecraft.getInstance().mainWindow.getScaledHeight()-15)/6),1111111);
         }
+
         back.renderButton(p_render_1_, p_render_2_, p_render_3_);
-        next.renderButton(p_render_1_, p_render_2_, p_render_3_);
 
 
     }
