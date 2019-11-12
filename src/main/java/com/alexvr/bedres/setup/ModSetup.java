5a3700b112d8fe72fd1d271d66f07f3ca203750b
AlexVR <josian.velez@upr.edu>
2019-11-12 17:09:32 -0400

    pedestal

--- src/main/java/com/alexvr/bedres/setup/ModSetup.java
+++ src/main/java/com/alexvr/bedres/setup/ModSetup.java
@@ -4,7 +4,8 @@ import com.alexvr.bedres.network.Networking;
 import com.alexvr.bedres.registry.ModBlocks;
 import com.alexvr.bedres.utils.RenderFactory;
 import net.minecraft.entity.merchant.villager.VillagerEntity;
-import net.minecraft.entity.passive.CowEntity;
+import net.minecraft.entity.monster.*;
+import net.minecraft.entity.passive.*;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
@@ -24,6 +25,17 @@ public class ModSetup {
         Networking.registerMessages();
         RenderingRegistry.registerEntityRenderingHandler(VillagerEntity.class, new RenderFactory("villager"));
         RenderingRegistry.registerEntityRenderingHandler(CowEntity.class, new RenderFactory("cow"));
+        RenderingRegistry.registerEntityRenderingHandler(CatEntity.class, new RenderFactory("cat"));
+        RenderingRegistry.registerEntityRenderingHandler(CreeperEntity.class, new RenderFactory("creeper"));
+        RenderingRegistry.registerEntityRenderingHandler(PigEntity.class, new RenderFactory("pig"));
+        RenderingRegistry.registerEntityRenderingHandler(SheepEntity.class, new RenderFactory("sheep"));
+        RenderingRegistry.registerEntityRenderingHandler(SkeletonEntity.class, new RenderFactory("skeleton"));
+        RenderingRegistry.registerEntityRenderingHandler(SpiderEntity.class, new RenderFactory("spider"));
+        RenderingRegistry.registerEntityRenderingHandler(ZombieEntity.class, new RenderFactory("zombie"));
+        RenderingRegistry.registerEntityRenderingHandler(ChickenEntity.class, new RenderFactory("chicken"));
+        RenderingRegistry.registerEntityRenderingHandler(IronGolemEntity.class, new RenderFactory("iron_golem"));
+        RenderingRegistry.registerEntityRenderingHandler(SquidEntity.class, new RenderFactory("squid"));
+        RenderingRegistry.registerEntityRenderingHandler(WitchEntity.class, new RenderFactory("witch"));
 
     }
 
--- src/main/java/com/alexvr/bedres/utils/RenderFactory.java
+++ src/main/java/com/alexvr/bedres/utils/RenderFactory.java
@@ -2,12 +2,10 @@ package com.alexvr.bedres.utils;
 
 import com.alexvr.bedres.biomes.decayingfluxed.DecayingFluxedBiome;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.CowRenderer;
-import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.entity.VillagerRenderer;
+import net.minecraft.client.renderer.entity.*;
 import net.minecraft.entity.merchant.villager.VillagerEntity;
-import net.minecraft.entity.passive.CowEntity;
+import net.minecraft.entity.monster.*;
+import net.minecraft.entity.passive.*;
 import net.minecraft.resources.IReloadableResourceManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
@@ -28,6 +26,28 @@ public class RenderFactory implements IRenderFactory {
             return new ModVillagerRender(manager);
         }else if (entity.contains("cow")) {
             return new ModCowRender(manager);
+        }else if (entity.contains("cat")) {
+            return new ModCatRender(manager);
+        }else if (entity.contains("creeper")) {
+            return new ModCreeperRender(manager);
+        }else if (entity.contains("pig")) {
+            return new ModPigRender(manager);
+        }else if (entity.contains("sheep")) {
+            return new ModSheepRender(manager);
+        }else if (entity.contains("skeleton")) {
+            return new ModSkeletonRender(manager);
+        }else if (entity.contains("spider")) {
+            return new ModSpiderRender(manager);
+        }else if (entity.contains("zombie")) {
+            return new ModZombieRender(manager);
+        }else if (entity.contains("chicken")) {
+            return new ModChickenRender(manager);
+        }else if (entity.contains("iron_golem")) {
+            return new ModIronGolemRender(manager);
+        }else if (entity.contains("squid")) {
+            return new ModSquidRender(manager);
+        }else if (entity.contains("witch")) {
+            return new ModWitchRender(manager);
         }
         return new ModVillagerRender(manager);
     }
@@ -72,5 +92,224 @@ public class RenderFactory implements IRenderFactory {
         }
     }
 
+    private class ModCatRender  extends CatRenderer {
+        private final ResourceLocation CAT_TEXTURES = new ResourceLocation("bedres:textures/entity/cat/cat.png");
+
+        public ModCatRender(EntityRendererManager renderManager) {
+            super(renderManager);
+
+        }
+
+        /**
+         * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+         */
+        protected ResourceLocation getEntityTexture(CatEntity entity) {
+            if (entity.world.getBiome(new BlockPos(entity.posX,entity.posY,entity.posZ)) instanceof DecayingFluxedBiome) {
+                return CAT_TEXTURES;
+            }else{
+                return super.getEntityTexture(entity);
+            }
+        }
+    }
+
+    private class ModCreeperRender  extends CreeperRenderer {
+        private final ResourceLocation CREEPER_TEXTURES = new ResourceLocation("bedres:textures/entity/creeper/creeper.png");
+
+        public ModCreeperRender(EntityRendererManager renderManager) {
+            super(renderManager);
+
+        }
+
+        /**
+         * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+         */
+        protected ResourceLocation getEntityTexture(CreeperEntity entity) {
+            if (entity.world.getBiome(new BlockPos(entity.posX,entity.posY,entity.posZ)) instanceof DecayingFluxedBiome) {
+                return CREEPER_TEXTURES;
+            }else{
+                return super.getEntityTexture(entity);
+            }
+        }
+    }
+
+    private class ModPigRender  extends PigRenderer {
+        private final ResourceLocation PIG_TEXTURES = new ResourceLocation("bedres:textures/entity/pig/pig.png");
+
+        public ModPigRender(EntityRendererManager renderManager) {
+            super(renderManager);
+
+        }
+
+        /**
+         * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+         */
+        protected ResourceLocation getEntityTexture(PigEntity entity) {
+            if (entity.world.getBiome(new BlockPos(entity.posX,entity.posY,entity.posZ)) instanceof DecayingFluxedBiome) {
+                return PIG_TEXTURES;
+            }else{
+                return super.getEntityTexture(entity);
+            }
+        }
+    }
+
+    private class ModSheepRender  extends SheepRenderer {
+        private final ResourceLocation COW_TEXTURES = new ResourceLocation("bedres:textures/entity/sheep/sheep.png");
+
+        public ModSheepRender(EntityRendererManager renderManager) {
+            super(renderManager);
+
+        }
+
+        /**
+         * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+         */
+        protected ResourceLocation getEntityTexture(SheepEntity entity) {
+            if (entity.world.getBiome(new BlockPos(entity.posX,entity.posY,entity.posZ)) instanceof DecayingFluxedBiome) {
+                return COW_TEXTURES;
+            }else{
+                return super.getEntityTexture(entity);
+            }
+        }
+    }
+
+    private class ModSkeletonRender  extends SkeletonRenderer {
+        private final ResourceLocation COW_TEXTURES = new ResourceLocation("bedres:textures/entity/skeleton/skeleton.png");
+
+        public ModSkeletonRender(EntityRendererManager renderManager) {
+            super(renderManager);
+
+        }
+
+        /**
+         * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+         */
+        protected ResourceLocation getEntityTexture(AbstractSkeletonEntity entity) {
+            if (entity.world.getBiome(new BlockPos(entity.posX,entity.posY,entity.posZ)) instanceof DecayingFluxedBiome) {
+                return COW_TEXTURES;
+            }else{
+                return super.getEntityTexture(entity);
+            }
+        }
+    }
+
+    private class ModSpiderRender  extends SpiderRenderer {
+        private final ResourceLocation COW_TEXTURES = new ResourceLocation("bedres:textures/entity/spider/spider.png");
+
+        public ModSpiderRender(EntityRendererManager renderManager) {
+            super(renderManager);
+
+        }
+
+        /**
+         * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+         */
+        protected ResourceLocation getEntityTexture(SpiderEntity entity) {
+            if (entity.world.getBiome(new BlockPos(entity.posX,entity.posY,entity.posZ)) instanceof DecayingFluxedBiome) {
+                return COW_TEXTURES;
+            }else{
+                return super.getEntityTexture(entity);
+            }
+        }
+    }
+
+    private class ModZombieRender  extends ZombieRenderer {
+        private final ResourceLocation COW_TEXTURES = new ResourceLocation("bedres:textures/entity/zombie/zombie.png");
+
+        public ModZombieRender(EntityRendererManager renderManager) {
+            super(renderManager);
+
+        }
+
+        /**
+         * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+         */
+        protected ResourceLocation getEntityTexture(ZombieEntity entity) {
+            if (entity.world.getBiome(new BlockPos(entity.posX,entity.posY,entity.posZ)) instanceof DecayingFluxedBiome) {
+                return COW_TEXTURES;
+            }else{
+                return super.getEntityTexture(entity);
+            }
+        }
+    }
+
+    private class ModChickenRender  extends ChickenRenderer {
+        private final ResourceLocation COW_TEXTURES = new ResourceLocation("bedres:textures/entity/chicken/chicken.png");
+
+        public ModChickenRender(EntityRendererManager renderManager) {
+            super(renderManager);
+
+        }
+
+        /**
+         * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+         */
+        protected ResourceLocation getEntityTexture(ChickenEntity entity) {
+            if (entity.world.getBiome(new BlockPos(entity.posX,entity.posY,entity.posZ)) instanceof DecayingFluxedBiome) {
+                return COW_TEXTURES;
+            }else{
+                return super.getEntityTexture(entity);
+            }
+        }
+    }
+
+    private class ModIronGolemRender  extends IronGolemRenderer {
+        private final ResourceLocation COW_TEXTURES = new ResourceLocation("bedres:textures/entity/iron_golem.png");
+
+        public ModIronGolemRender(EntityRendererManager renderManager) {
+            super(renderManager);
+
+        }
+
+        /**
+         * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+         */
+        protected ResourceLocation getEntityTexture(IronGolemEntity entity) {
+            if (entity.world.getBiome(new BlockPos(entity.posX,entity.posY,entity.posZ)) instanceof DecayingFluxedBiome) {
+                return COW_TEXTURES;
+            }else{
+                return super.getEntityTexture(entity);
+            }
+        }
+    }
+
+    private class ModSquidRender  extends SquidRenderer {
+        private final ResourceLocation COW_TEXTURES = new ResourceLocation("bedres:textures/entity/squid.png");
+
+        public ModSquidRender(EntityRendererManager renderManager) {
+            super(renderManager);
+
+        }
+
+        /**
+         * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+         */
+        protected ResourceLocation getEntityTexture(SquidEntity entity) {
+            if (entity.world.getBiome(new BlockPos(entity.posX,entity.posY,entity.posZ)) instanceof DecayingFluxedBiome) {
+                return COW_TEXTURES;
+            }else{
+                return super.getEntityTexture(entity);
+            }
+        }
+    }
+
+    private class ModWitchRender  extends WitchRenderer {
+        private final ResourceLocation COW_TEXTURES = new ResourceLocation("bedres:textures/entity/witch.png");
+
+        public ModWitchRender(EntityRendererManager renderManager) {
+            super(renderManager);
+
+        }
+
+        /**
+         * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
+         */
+        protected ResourceLocation getEntityTexture(WitchEntity entity) {
+            if (entity.world.getBiome(new BlockPos(entity.posX,entity.posY,entity.posZ)) instanceof DecayingFluxedBiome) {
+                return COW_TEXTURES;
+            }else{
+                return super.getEntityTexture(entity);
+            }
+        }
+    }
 
 }
