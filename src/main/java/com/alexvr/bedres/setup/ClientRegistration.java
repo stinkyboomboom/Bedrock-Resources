0fffa93dd04a2db7d9987df4e29c70f3691082f9
Josian Velez Ramos <josian.velez@upr.edu>
2019-12-13 16:37:47 -0400

    smoll fix for color registration

--- src/main/java/com/alexvr/bedres/setup/ClientRegistration.java
+++ src/main/java/com/alexvr/bedres/setup/ClientRegistration.java
@@ -1,7 +1,8 @@
 package com.alexvr.bedres.setup;
 
 import com.alexvr.bedres.BedrockResources;
-import com.alexvr.bedres.registry.ModItems;
+import com.alexvr.bedres.items.FluxedCreepEggItem;
+import com.alexvr.bedres.items.SporeDeityEggItem;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,8 +13,8 @@ public class ClientRegistration {
 
     @SubscribeEvent
     public static void onItemColor(ColorHandlerEvent.Item event) {
-        event.getItemColors().register((stack, i) -> 0x9e61d8, ModItems.fluxedCreepEggItem);
-        event.getItemColors().register((stack, i) -> 0xf11209, ModItems.sporeDeityEggItem);
+        event.getItemColors().register((stack, i) -> 0x9e61d8, new FluxedCreepEggItem());
+        event.getItemColors().register((stack, i) -> 0xf11209, new SporeDeityEggItem());
     }
 
 
